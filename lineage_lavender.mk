a64ea2bf07345ede325bbeb0281d4834089d6d88
Sensei-Developer <shoppingplay32@gmail.com>
2023-02-07 15:13:42 +0300

    lavender: Bringup to ExodusOS

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2022-2023 The ExodusOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,20 +9,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common ExodusOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# ExodusOS Properties
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_USES_AOSP_RECOVERY := true
+EXODUS_MAINTAINER := melles1991-&-Sensei-Developer
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := lineage_lavender
 PRODUCT_MODEL := Redmi Note 7
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_USES_AOSP_RECOVERY := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_QUICK_TAP := true
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
